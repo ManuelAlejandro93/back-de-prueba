69f55d8c5f8fb8e07c6570e3af8c977485392baf
clasefisica2019@gmail.com <clasefisica2019@gmail.com>
2024-03-17 21:29:32 -0500

    probando usuarios2

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 
+
 @Injectable()
 export class UsuariosService {
   create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+    // return 'This action adds a new usuario';
+    return 'gorda'
+    
   }
 
   findAll() {
